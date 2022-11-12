4f6d05fa5de63ee8e082b92f5fd60cdcd78d3e5b
aaquib <aaquib@wobb.ai>
2022-11-12 11:26:42 +0530

    new

---
+++ serviceworker.js
@@ -0,0 +1,6 @@
+//importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
+// importScripts("./sw2.js");
+importScripts(
+  "https://cdn.jsdelivr.net/npm/@suprsend/web-sdk@0.1.25/serviceworker/serviceworker.min.js"
+);
+init_workspace("YPBJIUdwbvZt542WDtyB");
